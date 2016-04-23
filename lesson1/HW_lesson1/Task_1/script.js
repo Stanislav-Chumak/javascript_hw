8dba0985c58af8dbf1e5dad46f6ad670e67c9f8c
Stanislav-Chumak <cdc.chumak@gmail.com>
2016-04-23 10:52:44 +0300

    JS-HW1_done

---
+++ lesson1/HW_lesson1/Task_1/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var name = 'Sergey';
+var developer;
+
+developer = name;
+
+alert(developer);
+
+
---
+++ lesson1/HW_lesson1/Task_2/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var name = prompt( 'Please, enter your name here', 'default' );
+
+alert('Ваше имя ' + name);
+
